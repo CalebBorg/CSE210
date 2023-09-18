2e7c5808d2443aff36efce979a5f339ea2a69842
Caleb Borg <bor22012@byui.edu>
2023-09-18 17:09:34 -0600

    Finished preps 3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Globalization;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+//DEFINE VARIABLES, INCLUDING randomGenerator
+        string play = "yes";
+        int magicNumber;
+        int guess = 0;
+        int numberOfGuesses = 0;
+        Random randomGenerator = new Random();
+
+//WHILE PLAYER WANTS TO KEEP PLAYING, CONTINUE THE GAME
+        while(play == "yes" || play == "Yes" || play == "YES")
+        {
+
+//DEFINE THE MAGIC NUMBER AS A NUMBER BETWEEN 1 AND 100
+            magicNumber = randomGenerator.Next(1,100);
+            Console.WriteLine(magicNumber);
+
+//WHILE THE GUESS AND THE MAGIC NUMBER ARE NOT THE SAME, KEEP GIVING HINTS
+            while(guess != magicNumber)
+            {
+                Console.WriteLine("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+
+//CHECK IF THE GUESS IS HIGHER THAN, LOWER THAN, OR EQUAL TO THE MAGIC NUMBER, THEN PRINT FEEDBACK
+                if(guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You did it! It took you {numberOfGuesses} guesses!");
+                }
+            }
+//CHECK IF USER STILL WANTS TO PLAY AND RESET GUESS COUNTER
+            Console.WriteLine("Do you want to contine playing? Type 'yes' to continue, type anything else to quit");
+            play = Console.ReadLine();
+            numberOfGuesses = 0;
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input = 1;
+        int sum = 0;
+        int listSize = 0;
+        int average = 0;
+        int largest = 0;
+        int smallest = 0;
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished");
+        while(input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+            listSize ++;
+            if (listSize > 0)
+            {
+                average = sum/listSize;
+                smallest = numbers[0];
+                largest = numbers[0];
+                if (number > largest)
+                {
+                    largest = number;
+                }
+
+                if (number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine($"Largest: {largest}");
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Your sorted list: ");
+        foreach (int number in numbers)
+        {
+            Console.Write($"{number} ");
+        }
     }
 }
