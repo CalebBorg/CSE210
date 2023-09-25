56c429c93da05e0e61db39b2ca8a7ed053c1994f
CalebBorg <144709504+CalebBorg@users.noreply.github.com>
2023-09-25 09:40:16 -0600

    Finished prepare learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1995;
+        job1._endYear = 2023;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Walmart";
+        job2._jobTitle = "Cashier";
+        job2._startYear = 20012;
+        job2._endYear = 2023;
+        job2.Display();
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Guy Mann";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
