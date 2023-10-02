04aae058266b2148fe10f3e774568d2010a2b367
CalebBorg <144709504+CalebBorg@users.noreply.github.com>
2023-10-02 16:45:39 -0600

    Update Develop 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,15 +9,15 @@ class Program
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 1995;
+        job1._startYear = 2012;
         job1._endYear = 2023;
         job1.Display();
 
         Job job2 = new Job();
         job2._company = "Walmart";
         job2._jobTitle = "Cashier";
-        job2._startYear = 20012;
-        job2._endYear = 2023;
+        job2._startYear = 1995;
+        job2._endYear = 2012;
         job2.Display();
 
 
---
+++ prepare/Learning02/Untitled-1.py
@@ -0,0 +1,17 @@
+animal = input("input an animal: ")
+location = input("input a place: ")
+noun = input("nount")
+adjective = input()
+
+
+
+
+print(f"""the {animal} drinks. then it goes to the {location}
+      
+ddidd 
+      
+gogog
+
+
+      """)
+
